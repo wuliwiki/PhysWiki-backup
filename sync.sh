dd8e528f6fd4784edb539f44d6671f871e339c65
MacroUniverse <270174408@qq.com>
2020-06-10 10:23:05 +0800

    amend

--- sync.sh
+++ sync.sh
@@ -1,5 +1,7 @@
+# to run in background, use `nohup ./sync.sh > /dev/null &`
+# to find the process, use `ps aux | grep sync.sh`
 while :
 do
 sleep 60;
-find . -type f -mmin -2 -exec cp {} /mnt/oss/PhysWiki-backup/ \;
+find . -type f -name '*.tex' -mmin -2 -exec cp {} /mnt/oss/PhysWiki-backup/ \;
 done
